e2f46276fe97923056b327b36a5f466a1b170744
Marina Zapasnik <zapasnikzapasnik@gmail.com>
2024-11-29 20:45:29 +0300

    commit

---
+++ exam-js-varibles-while/script01.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="style.css" />
+    <title>Document</title>
+  </head>
+  <body>
+    <h1>Содержание нашего курса 📚</h1>
+  </body>
+</html>
+
+
+//Какую строку надо добавить, чтобы подключить файл index.js
