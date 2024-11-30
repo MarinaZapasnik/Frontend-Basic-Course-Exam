2c2a6e4be2874bb814ee31f607b2b2cf5ba9c9a3
Marina Zapasnik <zapasnikzapasnik@gmail.com>
2024-11-30 12:24:25 +0300

    commit

---
+++ exam-js-if-else-switch/script03.js
@@ -0,0 +1,6 @@
+//Что нужно написать вместо XXX, чтобы в переменной status хранился текст 'активный', если переменная isLoggedIn равна true, и ‘неактивный’, если isLoggedIn равна false?
+
+
+
+let isLoggedIn = true
+const status = XXX
---
+++ exam-js-if-else-switch/script05.js
@@ -0,0 +1,19 @@
+//Кажется в коде чего-то не хватает. 
+
+//Какое значение будет в переменной action после выполнения данного кода?
+
+
+
+let batteryLevel = 'нет заряда'
+let action
+switch (batteryLevel) {
+  case 'нет заряда':
+    action = 1
+  case 'низкий':
+    action = 2
+  case 'полный':
+    action = 3
+}
+
+
+
